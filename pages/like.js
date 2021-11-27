3a4a642a3f43bea32b3b028c0e85383a329a6a9d
miishin <miishing@gmail.com>
2021-11-27 14:54:35 -0500

    grid menu

--- pages/like.js
+++ pages/like.js
@@ -8,31 +8,32 @@ export default function Ratings() {
     <div className="container">
       <NHead name="Ratings"/>
       <Header></Header>
-
-      <main className={styles.menu}>
       <div className={styles.linktxt}>∗: You want to know what I like?</div>
-        <div className={styles.items}>
+      <main className={styles.menu}>
           <div className={styles.menuItem}>
-            <img src="menu_item.svg"/>
+            <img src="menu_item.svg" className={styles.img}/>
             <a href = "https://rateyourmusic.com/~miishin" className={styles.linktxt}>Music</a>
           </div>
           <div className={styles.menuItem}>
-            <img src="menu_item.svg"/>
+            <img src="menu_item.svg" className={styles.img}/>
             <a href = "https://letterboxd.com/miishin/" className={styles.linktxt}>Movies</a>
           </div>
           <div className={styles.menuItem}>
-            <img src="menu_item.svg"/>
+            <img src="menu_item.svg" className={styles.img}/>
             <a href = "https://trakt.tv/users/miishin/ratings/all/all/highest" className={styles.linktxt}>TV Shows</a>
           </div>
           <div className={styles.menuItem}>
-            <img src="menu_item.svg"/>
-            <a href = "https://anilist.co/user/miishin/" className={styles.linktxt}>Anime + Manga</a>
+            <img src="menu_item.svg" className={styles.img}/>
+            <a href = "https://anilist.co/user/miishin/animelist" className={styles.linktxt}>Anime</a>
           </div>
           <div className={styles.menuItem}>
-            <img src="menu_item.svg"/>
+            <img src="menu_item.svg" className={styles.img}/>
+            <a href = "https://anilist.co/user/miishin/mangalist" className={styles.linktxt}>Manga</a>
+          </div>
+          <div className={styles.menuItem}>
+            <img src="menu_item.svg" className={styles.img}/>
             <a href = "https://glitchwave.com/user/miishin/" className={styles.linktxt}>Video Games</a>
           </div>
-        </div>
       </main>
     </div>
   )
