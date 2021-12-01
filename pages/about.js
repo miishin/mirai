a99dbd56c2f676bb2cfaa6a53e7da1692c3ca0db
miishin <miishing@gmail.com>
2021-12-01 01:07:23 -0500

    cleaned up CSS positioning, About page, and restructured menus

--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,26 @@
 import Head from 'next/head'
 import Header from '../components/header.js'
 import NHead from '../components/head.js'
-import styles from '../styles/About.module.css'
+import styles from '../styles/Menu.module.css'
 
 export default function About() {
   return (
     <div className="container">
       <NHead>About</NHead>
 
-      <Header></Header>
+      <Header name="About"/>
 
-      <main className={styles.contents}>
-        <div className={styles.items}>
-          <div className={styles.txt}>
-              <p>Mirai.</p>
-          </div>
-        </div>
+      <main className={styles.menu}>
+      <img src="textbox.svg"/>
+      <div className={styles.items}>
+      <div className={styles.linktxt}>Here are my social media links.</div>
+        <a href="https://linkedin.com/in/miraisahara">
+          <div className={styles.linktxt}>∗ My LinkedIn</div>
+        </a>
+        <a href="https://github.com/miishin">
+          <div className={styles.linktxt}>∗ My GitHub</div>
+        </a>
+      </div>
       </main>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,6 @@ export default function Home() {
   return (
     <div className="container">
       <NHead name="Home"/>
-
       <Header></Header>
 
       <main className={styles.menu}>
