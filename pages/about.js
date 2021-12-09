073eb8addb75d10c769261c15879ac8eafe3358a
miishin <miishing@gmail.com>
2021-12-08 21:13:30 -0500

    fix about textbox and textbox name

--- pages/about.js
+++ pages/about.js
@@ -2,23 +2,26 @@ import Head from 'next/head'
 import Header from '../components/header.js'
 import NHead from '../components/head.js'
 import styles from '../styles/Menu.module.css'
+import gridstyles from '../styles/GridMenu.module.css'
 
 export default function About() {
   return (
     <div className="container">
-      <NHead>About</NHead>
+      <NHead>About Mii</NHead>
 
       <Header name="About"/>
 
       <main className={styles.menu}>
       <img src="textbox.svg"/>
       <div className={styles.items}>
-      <div className={styles.linktxt}>Here are my social media links.</div>
+        <div className={styles.linktxt}>
+          My name's Mirai, but my nickname is
+        </div>
+        <div className={styles.linktxt}>
+          "miishin" everywhere online.
+        </div>
         <a href="https://linkedin.com/in/miraisahara">
           <div className={styles.linktxt}>∗ My LinkedIn</div>
-        </a>
-        <a href="https://github.com/miishin">
-          <div className={styles.linktxt}>∗ My GitHub</div>
         </a>
       </div>
       </main>
